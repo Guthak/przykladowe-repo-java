b704a8bf2108a797cd4a1e5e244cae6d2359c2e4
Guthak <tomekomen1@gmail.com>
2021-10-24 13:07:15 +0200

    Zrobione

---
+++ src/Zadanie7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Zadanie7 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Podaj liczbę do wzoru");
+
+        Scanner scanner = new Scanner(System.in);
+        int fibonacci = scanner.nextInt();
+        int a = 0;
+        int b = 1;
+
+
+        int i = 1;
+        while (i <= fibonacci) {
+
+            int sum = a + b;
+            a = b;
+            b = sum;
+            i++;
+
+            System.out.print(sum + " ");
+        }
+
+
+    }
+}
+
+
+
